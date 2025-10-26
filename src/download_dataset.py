b83fd03dd626cb94928772468cd53fcee7efa263
pradanaadn <pradanaadn21@gmail.com>
2025-10-26 18:58:14 +0800

    feat: update .gitignore to exclude datasets and modify download_dataset.py for dataset download

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -5,5 +5,5 @@ import kagglehub
 
 data_path = Path(__file__).parent.parent
 os.environ["KAGGLEHUB_CACHE"] = str(data_path)
-
+path = kagglehub.dataset_download("saurav9786/amazon-product-reviews")
 print(data_path)
